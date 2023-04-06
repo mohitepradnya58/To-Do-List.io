3746637391e235c051244a868fae72645027b5ee
mohiteankita <127580118+mohitepradnya58@users.noreply.github.com>
2023-04-06 17:20:46 +0530

    to do list

---
+++ index.js
@@ -0,0 +1,19 @@
+let inputField = document.getElementById('inputField');
+let addToDoButton = document.getElementById('addToDoButton');
+let toDoContainer = document.getElementById('toDoContainer');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+   
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerHTML = inputField.value;
+
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+ paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration = 'line-through';
+})
+paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph); 
+}) 
+});
